eb0ad629845e5384da6c2d3dac112b68c30ca4d4
Willy Abner <willabner.radical@gmail.com>
2016-05-28 21:52:13 -0500

    agregando validaciones en el login

---
+++ JS/Login.js
@@ -0,0 +1,15 @@
+$(function (){
+  function validarFields(user,pass){
+    if(user.length>1 && user.length||pass.length>1 && pass.length<50){
+
+    }else{alert("LOS CAMPOS ESTAN VACIOS"); }
+
+  }
+$('login').click(function(e){
+
+    e.preventDefault();
+    var usuario=$('user').val();
+    var clave=$('pass').val();
+    alert(validarFields(usuario,clave));
+});
+});

e21943fbf42eff1beb64907883a43d7c7992968e
Willy Abner <willabner.radical@gmail.com>
2016-05-29 18:54:20 -0500

    agregue css y codigo para el buscador por codigos

--- JS/Login.js
+++ JS/Login.js
@@ -2,7 +2,7 @@ $(function (){
   function validarFields(user,pass){
     if(user.length>1 && user.length||pass.length>1 && pass.length<50){
 
-    }else{alert("LOS CAMPOS ESTAN VACIOS"); }
+    }else{alert("Cajas Vacias"); }
 
   }
 $('login').click(function(e){
